5f25ed954d3a89e9b65442d02dc0854d82583872
netomachado <netomachado72@gmail.com>
2021-07-08 20:54:26 -0300

    instalando o multer

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const multer = require('multer');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
